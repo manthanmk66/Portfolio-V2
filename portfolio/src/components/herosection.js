e83915a38cb5adf6f1fefe58c4a518c1ef0c7a68
Manthan Kasle <manthankaslemk@gmail.com>
2024-06-21 12:30:07 +0530

    Added Herosection title

---
+++ portfolio/src/components/herosection.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section
+      id="work"
+      className=" gap-4 p-4"
+      data-aos="fade-up"
+      data-aos-delay="400"
+    >
+      <div className="flex items-center justify-center md:justify-start md:col-span-1">
+        <div className="flex flex-col items-center md:flex-row">
+          <p className="jello text-3xl">M</p>
+          <p className="jello text-3xl">a</p>
+          <p className="jello text-3xl">n</p>
+          <p className="jello text-3xl">t</p>
+          <p className="jello text-3xl">h</p>
+          <p className="jello text-3xl">a</p>
+          <p className="jello text-3xl">n</p>
+          &nbsp;
+          <p className="jello text-3xl">K</p>
+          <p className="jello text-3xl">a</p>
+          <p className="jello text-3xl">s</p>
+          <p className="jello text-3xl">l</p>
+          <p className="jello text-3xl">e</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-center md:justify-start md:col-span-1">
+        <p className="jello text-3xl">I</p>
+        <p className="jello text-3xl">&nbsp;d</p>
+        <p className="jello text-3xl">e</p>
+        <p className="jello text-3xl">s</p>
+        <p className="jello text-3xl">i</p>
+        <p className="jello text-3xl">g</p>
+        <p className="jello text-3xl">n</p>
+        &nbsp;
+        <p className="jello text-3xl">w</p>
+        <p className="jello text-3xl">e</p>
+        <p className="jello text-3xl">b</p>
+        &nbsp;
+        <p className="jello text-3xl">a</p>
+        <p className="jello text-3xl">n</p>
+        <p className="jello text-3xl">d</p>
+        &nbsp;
+        <p className="jello text-3xl">c</p>
+        <p className="jello text-3xl">o</p>
+        <p className="jello text-3xl">d</p>
+        <p className="jello text-3xl">e</p>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
